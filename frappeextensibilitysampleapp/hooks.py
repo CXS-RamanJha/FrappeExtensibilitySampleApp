70bfa663cded942d4ab8fc9809579a576c75ed0b
Raman <raman.jha@citixsys.com>
2025-02-12 06:16:32 +0000

    Initial commit of Frappe Extensibility Sample App

--- frappeextensibilitysampleapp/hooks.py
+++ frappeextensibilitysampleapp/hooks.py
@@ -6,7 +6,7 @@ app_email = "cd@citisxys.com"
 app_license = "mit"
 
 # Apps
-# ------------------frappeextensibilitysampleapp
+# ------------------frappeextensibilitysampleapp1
 
 # required_apps = []
 
